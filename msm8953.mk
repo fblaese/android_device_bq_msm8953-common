7cf50664ca715af76d3c1f92a5251606058519ee
Quallenauge <Hamsi2k@freenet.de>
2019-05-11 23:30:31 +0200

    Revert "camera: Add missing Fence destructor"
    This prepares the CAF camera HAL sources.
    
    This reverts commit 6e238c54402b69c49f80a7bded57cfd01508ed93.
    
    Change-Id: Ia0f977adb209a2c61a0b77c95eebf3294837fcff

--- msm8953.mk
+++ msm8953.mk
@@ -127,8 +127,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap \
-    libshims_camera
+    Snap
 
 # mct_pipeline
 PRODUCT_COPY_FILES += \
